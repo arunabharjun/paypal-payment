7734dc1f3a12ed297d80adbddac180e0fbfdc135
arunabharjun <sunnyarj114@gmail.com>
2020-10-24 01:04:33 +0530

    Adding the create/product api with PayPal catalog API

--- controllers/apiControllers.js
+++ controllers/apiControllers.js
@@ -13,33 +13,46 @@ const catelogAPI = `${api}/${CATELOGS_ENDPOINT}`;
 exports.createProduct = (req, res) => {
 	const name = req.body.pname;
 	const type = req.body.type;
-	axios
-		.post(catelogAPI, {
-			headers: {
-				Authorization: `Basic ${CLIENT_ID}:${SECRET}`,
-				'Content-Type': 'application/json'
-			},
-			data: {
-				name,
-				type
-			}
-		})
-		.then((response) => {
-			console.log('Success');
-			res.json({
-				response
-			});
-		})
-		.catch((error) => {
-			console.log('Error');
-			res.json({
-				error
-			});
+
+	if (!name) {
+		res.json({
+			error: 'Product name is a required field.'
 		});
+	}
+	else if (!type) {
+		res.json({
+			error: 'Product type is a required field.'
+		});
+	}
+	else {
+		axios
+			.post(catelogAPI, {
+				headers: {
+					Authorization: `Basic ${CLIENT_ID}:${SECRET}`,
+					'Content-Type': 'application/json'
+				},
+				data: {
+					name,
+					type
+				}
+			})
+			.then((response) => {
+				console.log('Success');
+				res.json({
+					response
+				});
+			})
+			.catch((error) => {
+				console.log('Error');
+				res.json({
+					error
+				});
+			});
+	}
 };
 
 /**
- * Demo
+ * Test API Demo
  */
 exports.api = (req, res) => {
 	res.json({
